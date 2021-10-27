e4143443859b34d22afc50fd95f273a6fd29680f
carla <vila.carla@yahoo.fr>
2021-10-27 14:22:56 +0200

    ajout des tokens

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]; // <- à préciser dans notre requête headers: {Authorization : type Bearer}
-        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY); // <- notre TOKEN provisoire (à remplacer par process.env.TOKEN ensuite)
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY); 
         if (!decodedToken.userId) {
             throw 'Token invalide!';
         } else {
--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -5,7 +5,7 @@ const User = db.User
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]; // <- à préciser dans notre requête headers: {Authorization : type Bearer}
-        const decodedToken = jwt.verify(token, 'token_dev'); // <- notre TOKEN provisoire (à remplacer par process.env.TOKEN ensuite)
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY); 
     
         /* Protégeons ce compte en vérifiant si le userId du token correspond au id du compte sur lequel on souhaite accéder */
         User.findOne({where: {id: req.params.id}})
