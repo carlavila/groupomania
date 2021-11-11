37ed5feff6227181a3024de34b30290c3ccda7f0
carla <vila.carla@yahoo.fr>
2021-11-11 10:17:47 +0100

    mise en page

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -7,17 +7,22 @@ const auth = require('../middleware/auth.js');
 //CONTROLLERS
 const commentCtrl = require('../controllers/comment.js')
 
-///Routes
+///ROUTES
+
 /* '/' = https://localhost:3000/api/comments */
 
 /* route création commentaire */
 router.post('/create', auth, commentCtrl.createComment);
+
 /* route affichage ensemble des commentaires */
 router.get('/:postId/all', commentCtrl.getAllComments);
+
 /* route affichage d'un commentaire en particulier */
 router.get('/:id', commentCtrl.getOneComment);
+
 /* route modification commentaire */
 router.put('/:id', auth, commentCtrl.editComment);
+
 /* route suppression commentaire */
 router.delete('/:id', auth, commentCtrl.deleteComment);
 
